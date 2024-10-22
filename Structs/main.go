aa192c8e7beb4a3fc39aeaa4ee3d583bd18c93fb
Fahad Mahmood <fahadmahmood@Fahads-MacBook-Air.local>
2024-10-22 12:40:19 +0530

    Structs

--- Structs/main.go
+++ Structs/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	// No inheratance is been allowed in GO
+
+	details := User{"Fahad", "fahad@go.dev", true, 16}
+
+	fmt.Println(details)
+	fmt.Printf("Details %+v", details)
+	fmt.Printf("Name %v \n", details.Name)
+}
+
+type User struct {
+	Name   string
+	Email  string
+	Status bool
+	Age    int
 }
